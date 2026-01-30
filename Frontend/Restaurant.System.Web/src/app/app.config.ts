038329a80173b3cc73d1ca205aa4248afefe1b33
LeeZX24 <leezixiang24@gmail.com>
2026-01-30 09:17:20 +0000

    fix lint problemn

--- Frontend/Restaurant.System.Web/src/app/app.config.ts
+++ Frontend/Restaurant.System.Web/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor, JwtInterceptor } from './core/interceptor/shared.inceptor';
 import { provideTranslateService } from '@ngx-translate/core';
 import { LoadingInterceptor } from './core/interceptor/loading.interceptor';
