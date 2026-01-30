66f2f9c804e4c179deb0d3830e673056e7c19f6a
LeeZX24 <leezixiang24@gmail.com>
2026-01-30 09:41:18 +0000

    fix test problem

--- Frontend/Restaurant.System.Web/src/auth/login/login.component.spec.ts
+++ Frontend/Restaurant.System.Web/src/auth/login/login.component.spec.ts
@@ -2,16 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { provideHttpClient } from '@angular/common/http';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { provideAppInitializer, provideZonelessChangeDetection } from '@angular/core';
+import { loadAppConfig } from '../../app/utils/runtime-env';
+import { APP_CONFIG } from '../../app/shared/configs/app-config.token';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const mockAppConfig = {
+    apiUrl: 'http://localhost:3000',
+    // ... add other properties your config requires
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginComponent],
-      providers: [provideZonelessChangeDetection(), provideHttpClient()]
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        {
+          provide: APP_CONFIG,
+          useValue: mockAppConfig
+        }
+      ]
     })
     .compileComponents();
 
--- Frontend/Restaurant.System.Web/src/auth/register/register.component.spec.ts
+++ Frontend/Restaurant.System.Web/src/auth/register/register.component.spec.ts
@@ -2,16 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 import { provideHttpClient } from '@angular/common/http';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { provideAppInitializer, provideZonelessChangeDetection } from '@angular/core';
+import { loadAppConfig } from '../../app/utils/runtime-env';
+import { APP_CONFIG } from '../../app/shared/configs/app-config.token';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  const mockAppConfig = {
+    apiUrl: 'http://localhost:3000',
+    // ... add other properties your config requires
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RegisterComponent],
-      providers: [provideZonelessChangeDetection(), provideHttpClient()]
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        {
+          provide: APP_CONFIG,
+          useValue: mockAppConfig
+        }
+      ]
     })
     .compileComponents();
 
