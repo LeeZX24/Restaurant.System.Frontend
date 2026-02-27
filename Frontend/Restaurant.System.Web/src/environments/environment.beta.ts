708a69e7ec6a37836d5913afee5d2a1ef58d976f
Lee Zi Xiang <zixiang.lee@consolsys.com>
2026-02-27 14:51:02 +0800

    fixes

--- Frontend/Restaurant.System.Web/src/environments/environment.beta.ts
+++ Frontend/Restaurant.System.Web/src/environments/environment.beta.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   version: '__APP_VERSION__',
   channel: 'beta',
-  baseUrl: 'https://restaurant-system-jtp3.onrender.com/'
+  baseUrl: 'https://restaurant-system-jtp3.onrender.com'
 };
