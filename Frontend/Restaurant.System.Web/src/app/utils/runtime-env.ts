2734bb8ccabda556c2b9981e3dd9ff9262d999c1
LeeZX24 <leezixiang24@gmail.com>
2026-04-24 07:47:41 +0000

    fixes

--- Frontend/Restaurant.System.Web/src/app/utils/runtime-env.ts
+++ Frontend/Restaurant.System.Web/src/app/utils/runtime-env.ts
@@ -1,28 +1,33 @@
 import { AppConfig } from '../shared/configs/app-config.model';
 import { environment } from '../../environments/environment';
+import { inject, isDevMode, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 export async function loadAppConfig(): Promise<AppConfig> {
-  // const platformId = inject(PLATFORM_ID);
-  // const http = inject(HttpClient);
+  const platformId = inject(PLATFORM_ID);
+  const http = inject(HttpClient);
 
-  // let cfg: AppConfig = { baseUrl: environment.baseUrl };
+  let cfg: AppConfig = { baseUrl: environment.baseUrl };
 
-  // if (isPlatformBrowser(platformId)) {
-  //   const loc = window.location;
-  //   if (isDevMode()) {
-  //     if(loc.port != '5000') {
-  //       if(loc.hostname !== 'localhost'){
-  //         cfg = { baseUrl: `https://${window.location.hostname.replace('4200', '5000')}`}
-  //       } else {
-  //         cfg = { baseUrl: `${window.location.origin.replace('4200', '5000')}`};
-  //       }
-  //     } else {
-  //   const response = await firstValueFrom(http.get<AppConfig>('assets/config.json'));
-  //   if (!response) throw new Error('Dev config not found or invalid');
-  //   cfg = response;
-  //   }
-  //   }
-  // }
+  if (isPlatformBrowser(platformId)) {
+    const loc = window.location;
+    if (isDevMode()) {
+      if (loc.port != '5000') {
+        if (loc.hostname !== 'localhost') {
+          cfg = { baseUrl: `https://${window.location.hostname.replace('4200', '5000')}` };
+        } else {
+          cfg = { baseUrl: `${window.location.origin.replace('4200', '5000')}` };
+        }
+      } else {
+        const response = await firstValueFrom(http.get<AppConfig>('assets/config.json'));
+        if (!response) throw new Error('Dev config not found or invalid');
 
-  return { baseUrl: environment.baseUrl };
+        cfg = response;
+      }
+    }
+  }
+
+  return cfg;
 }
