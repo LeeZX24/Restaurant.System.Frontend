7ca68fa39a1660440930592fb6c382ffe9bb7e35
LeeZX24 <leezixiang24@gmail.com>
2026-09-25 08:40:13 +0000

    update build commands

---
+++ Restaurant.System.Web/src/assets/scripts/build-beta.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# =========================
+# Beta
+# =========================
+
+rm -f src/assets/app-meta.js
+
+git fetch --tags --force
+git checkout origin/beta
+
+META_VERSION=$(git describe --tags --match "v*-beta.*" --abbrev=0 || echo "0.0.0-build.0")
+BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+CHANNEL="beta"
+
+cat > src/assets/app-meta.js <<EOF
+window.APP_META = {
+  version: "$META_VERSION",
+  channel: "$CHANNEL",
+  buildTime: "$BUILD_TIME"
+};
+EOF
+
+CI=true yarn bb -- --output-path=dist/beta
---
+++ Restaurant.System.Web/src/assets/scripts/build-release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# =========================
+# Release
+# =========================
+
+rm -f src/assets/app-meta.js
+
+git fetch --tags --force
+git checkout origin/release
+
+META_VERSION=$(git describe --tags --abbrev=0 || echo "0.0.0")
+BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+CHANNEL="release"
+
+cat > src/assets/app-meta.js <<EOF
+window.APP_META = {
+  version: "$META_VERSION",
+  channel: "$CHANNEL",
+  buildTime: "$BUILD_TIME"
+};
+EOF
+
+CI=true yarn br -- --output-path=dist/release
