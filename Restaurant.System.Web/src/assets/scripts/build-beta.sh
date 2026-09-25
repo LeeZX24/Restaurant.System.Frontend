9d1ea6ad8c6dc704cf65efeb2a48f5bb548f5d3c
LeeZX24 <leezixiang24@gmail.com>
2026-09-25 16:55:23 +0800

    Update build-beta.sh for improved git commands

--- Restaurant.System.Web/src/assets/scripts/build-beta.sh
+++ Restaurant.System.Web/src/assets/scripts/build-beta.sh
@@ -7,8 +7,8 @@ set -euo pipefail
 
 rm -f src/assets/app-meta.js
 
-git fetch --tags --force
-git checkout origin/beta
+git fetch origin beta --tags --force
+git checkout --detach origin/beta
 
 META_VERSION=$(git describe --tags --match "v*-beta.*" --abbrev=0 || echo "0.0.0-build.0")
 BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
